4f5009bedb96047e6b8c24bedaed18d05180014e
Aidan Timson <contact@timmo.xyz>
2022-09-18 14:34:11 +0100

    Add command

--- module/pythontemplate/__main__.py
+++ module/pythontemplate/__main__.py
@@ -8,6 +8,12 @@ from ._version import __version__
 app = typer.Typer()
 
 
+@app.command(name="hello", short_help="Say Hello World")
+def hello_world() -> None:
+    """Say Hello World"""
+    typer.secho("Hello world", fg=typer.colors.GREEN)
+
+
 @app.command(name="version", short_help="Module Version")
 def version() -> None:
     """Module Version"""
