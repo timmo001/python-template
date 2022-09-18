546f6f5bb0227165386bd4e8a4a4064d69eaebf1
Aidan Timson <contact@timmo.xyz>
2022-09-18 15:01:13 +0100

    Version 1.0.0

--- module/timmopytemplate/_version.py
+++ module/timmopytemplate/_version.py
@@ -7,5 +7,5 @@ Provides timmopytemplate version information.
 
 from incremental import Version
 
-__version__ = Version("timmopytemplate", 1, 0, 0, dev=2)
+__version__ = Version("timmopytemplate", 1, 0, 0)
 __all__ = ["__version__"]
